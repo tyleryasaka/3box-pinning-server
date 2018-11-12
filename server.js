f58c3bb5acfcbbaa8dd1e2b1221a663ba70eae4d
Tyler Yasaka <contact@tyleryasaka.me>
2018-11-12 12:16:06 -0800

    Use orbit-db-ethstore for root store

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const IPFS = require('ipfs')
 const OrbitDB = require('orbit-db')
 const Pubsub = require('orbit-db-pubsub')
+const OrbitDBEthstore = require('orbit-db-ethstore')
 
 const ipfsOptions = {
   EXPERIMENTAL: {
@@ -21,6 +22,8 @@ let openDBs = {}
 ipfs.on('error', (e) => console.error(e))
 ipfs.on('ready', async () => {
   console.log(await ipfs.id())
+  const keystore = new OrbitDBEthstore(process.env['WEB3_PROVIDER'])
+  const rootOrbitdb = new OrbitDB(ipfs, ORBITDB_PATH, { keystore })
   const orbitdb = new OrbitDB(ipfs, ORBITDB_PATH)
   const pubsub = new Pubsub(ipfs, (await ipfs.id()).id)
 
@@ -28,7 +31,7 @@ ipfs.on('ready', async () => {
 
   async function openRootDB (address) {
     if (!openDBs[address]) {
-      openDBs[address] = await orbitdb.open(address)
+      openDBs[address] = await rootOrbitdb.open(address)
       openDBs[address].events.on('ready', () => {
         openStoresAndSendResponse()
       })
